df6fe520d6b8eda0210670ef7ca27acebba7a723
Nicole <nicolefeng7@gmail.com>
2023-08-14 12:26:46 -0400

    Add skeleton code for generate mesh function

---
+++ mesh.cpp
@@ -0,0 +1,10 @@
+/*
+ * Generate a mesh with a square linkage pattern, with dimensions N x M (where N = number of rows of squares, M = number
+ * of columns.)
+ *
+ * Return the vertex positions and face vertex indices in the matrices V and F, respectively.
+ */
+void generateSquarePatternMesh(size_t N, size_t M, Eigen::MatrixXd& V, Eigen::MatrixXi& F) {}
+
+
+/* TODO: Feel free to generate other linkage patterns, such as rectangle, parallelogram, etc.! */
